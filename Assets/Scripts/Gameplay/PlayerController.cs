1e4bddda60842977d4aa848288ea1bf1cee685f8
AlexandruAfrasinei@gmail.com <AlexandruAfrasinei@gmail.com>
2016-04-16 14:33:16 +0300

    Added moving player placeholder

---
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Vector2 velocity;
+    public Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime);
+    }
+}
