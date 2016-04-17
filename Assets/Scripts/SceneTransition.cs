4b2d108d712d7cac7f24099dff5501991bed4852
Marius Petcu <dapetcu21@gmail.com>
2016-04-17 23:42:12 +0300

    Implement fade background

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition
+{
+    private AsyncOperation _sceneLoading;
+
+    private SceneTransition(AsyncOperation sceneLoading)
+    {
+        _sceneLoading = sceneLoading;
+        _sceneLoading.allowSceneActivation = false;
+
+        GameObject[] gameObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+        foreach (GameObject obj in gameObjects) {
+            if (obj.CompareTag("FadeBackground")) {
+                obj.GetComponent<FadeBackground>().StartFade(this);
+            }
+        }
+    }
+
+    public IEnumerator OnEndFade()
+    {
+        _sceneLoading.allowSceneActivation = true;
+        yield return _sceneLoading;
+    }
+
+    static public void TransitionToScene(int index)
+    {
+        new SceneTransition(SceneManager.LoadSceneAsync(index));
+    }
+
+    static public void TransitionToScene(string name)
+    {
+        new SceneTransition(SceneManager.LoadSceneAsync(name));
+    }
+}
