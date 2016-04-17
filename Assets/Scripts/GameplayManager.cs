71ebbdfe13fec7436c1378c7cdb202086a67b241
dumytru <dumitru.nemtuc@gmail.com>
2016-04-18 00:22:19 +0300

    added menu logic and save progress

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using System;
 
 public class GameplayManager : MonoBehaviour
 {
@@ -11,7 +11,18 @@ public class GameplayManager : MonoBehaviour
 	{
 		instance = this;
 		DatabaseManager.Initialize(gameSettings);
-	}
+
+        //level progress logic
+        _levelStars.Clear();
+        for (int i = 0; i < GameplayManager.instance.levelCount; i++)
+        {
+            _levelStars.Add(0);
+        }
+        _unlockedCount = 1;
+        
+        //load the progress at start
+        LoadProgress();
+    }
 	/************* singleton *************/
 
 
@@ -30,15 +41,25 @@ public class GameplayManager : MonoBehaviour
     private float _beforeTimeScale;
 
     private float startTime;
+    
+    private WinControl _winControl;
+    private Animator _winAnim;
 
     List<RopeController> _ropeControllers = new List<RopeController>();
 
+    //level logic
+    private int _unlockedCount = 1;
+    private List<int> _levelStars = new List<int>();
+    
     void Start()
     {
         _isMuted = false;
         _isPaused = false;
         Time.timeScale = 1.0f;
         startTime = Time.time;
+        
+        _winControl = winPanel.GetComponent<WinControl>();
+        _winAnim = winPanel.GetComponent<Animator>();
     }
 
 	void Update () {
@@ -60,10 +81,24 @@ public class GameplayManager : MonoBehaviour
 
         //update and save progress
         int starCount = GetStarCount();
+        //minus one because arrays starts at 0 and level names are from 1
+        int levelIndex = GetLevelIndex();
+        
+        //warning log
+        if(levelIndex > levelCount)
+        {
+            Debug.LogWarning("Problem with level index. It's greater than the level count (set in GameplayManager)");
+        }
+
+        //update stuff in level control
+        UpdateLevelStar(levelIndex-1, starCount);
+        UpdateUnlockedCount(levelIndex);
+        //and save the progress on disk
+        SaveProgress();
 
         //update and activate win panel
-        winPanel.GetComponent<WinControl>().SetStars(GetStarCount());
-        winPanel.GetComponent<Animator>().SetTrigger("PopUp");
+        _winControl.SetStars(GetStarCount());
+        _winAnim.SetTrigger("PopUp");
     }
 
     public void Lose()
@@ -144,11 +179,80 @@ public class GameplayManager : MonoBehaviour
         if (playTime <= twoStarsTargetTime * threeStarsPercent / 100.0f) { return 3; }
         return 2;
     }
-
-    /*
+    
     public int GetLevelIndex()
     {
-        SceneManager.GetActiveScene();
+        string levelIndexStr = SceneManager.GetActiveScene().name.Substring(5);
+        return Int32.Parse(levelIndexStr);
     }
-    */
+
+    public void LoadProgress()
+    {
+        if (!HadProgress())
+        {
+            return;
+        }
+
+        //load unlocked levels count
+        _unlockedCount = PlayerPrefs.GetInt("UnlockedCount");
+
+        //load stars for each level
+        for (int i = 0; i < GameplayManager.instance.levelCount; i++)
+        {
+            _levelStars[i] = PlayerPrefs.GetInt("LevelStar" + i);
+        }
+
+        print("Unlocked count: " + _unlockedCount);
+    }
+
+    public void SaveProgress()
+    {
+        PlayerPrefs.SetInt("HasProgress", 1);
+
+        PlayerPrefs.SetInt("UnlockedCount", _unlockedCount);
+
+        for (int i = 0; i < GameplayManager.instance.levelCount; i++)
+        {
+            PlayerPrefs.SetInt("LevelStar" + i, _levelStars[i]);
+        }
+    }
+
+    public void ResetProgress()
+    {
+        PlayerPrefs.SetInt("HasProgress", 1);
+        PlayerPrefs.SetInt("UnlockedCount", 1);
+
+        for (int i = 0; i < GameplayManager.instance.levelCount; i++)
+        {
+            PlayerPrefs.SetInt("LevelStar" + i, 0);
+        }
+    }
+
+    public bool HadProgress()
+    {
+        return PlayerPrefs.HasKey("HasProgress");
+    }
+
+    public void UpdateUnlockedCount(int lastPassedLevel)
+    {
+        if (lastPassedLevel+1 > _unlockedCount)
+        {
+            _unlockedCount = lastPassedLevel+1;
+        }
+    }
+
+    public void UpdateLevelStar(int levelIndex, int starCount)
+    {
+        if (starCount < 0 || starCount > 3)
+        {
+            return;
+        }
+
+        if(_levelStars[levelIndex] < starCount)
+            _levelStars[levelIndex] = starCount;
+    }
+
+    public List<int> GetLevelStars() { return _levelStars; }
+    public int GetUnlockedCount() { return _unlockedCount; }
+    public int GetLevelCount() { return levelCount; }
 }
---
+++ Assets/Scripts/LevelBrowser.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelBrowser : MonoBehaviour {
+    public GameObject levelSelector;
+    public GameObject levelBrowserItem;
+    
+    private List<int> _levelStars;
+    private int _unlockedCount;
+    private int _levelCount;
+
+    void Start ()
+    {
+        _levelStars = GameplayManager.instance.GetLevelStars();
+        _unlockedCount = GameplayManager.instance.GetUnlockedCount();
+        _levelCount = GameplayManager.instance.GetLevelCount();
+
+        int i;
+        GameObject currentItem;
+
+        for(i = 0; i < _levelCount; i++)
+        {
+            currentItem = (GameObject)Instantiate(levelBrowserItem);
+            currentItem.transform.SetParent(levelSelector.transform);
+            currentItem.GetComponent<LevelBrowserItem>().UpdateStars(_levelStars[i], (_unlockedCount > i) ? false : true, i+1);
+        }
+    }
+}
