705e31ff51aa298b82608903270a0665f7209085
dumytru <dumitru.nemtuc@gmail.com>
2016-04-17 14:33:08 +0300

    changed win/pause menu, added stars, fixed mute button

--- Assets/Scripts/Buttons/WinControl.cs
+++ Assets/Scripts/Buttons/WinControl.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class WinControl : MonoBehaviour {
+
+    public List<Image> stars;
+    public Sprite activeStar;
+    public Sprite blankStar;
 
     public void NextLevel()
     {
@@ -11,5 +17,20 @@ public class WinControl : MonoBehaviour {
     public void MainMenu()
     {
         GameplayManager.instance.MainMenu();
+    }
+
+    public void SetStars(int count)
+    {
+        int i;
+
+        for(i = 0; i < count && i < stars.Count; i++)
+        {
+            stars[i].sprite = activeStar;
+        }
+
+        for(; i < stars.Count; i++)
+        {
+            stars[i].sprite = blankStar;
+        }
     }
 }
