f6a988572e3dbf907d6b55e69baba1453eab456b
Marius Petcu <dapetcu21@gmail.com>
2016-04-18 19:59:58 +0300

    Add touch support

--- Assets/Scripts/Gameplay/RopeController.cs
+++ Assets/Scripts/Gameplay/RopeController.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class RopeController : MonoBehaviour {
-	private List<Vector2> _vertices;
+public class RopeController : MonoBehaviour
+{
+    const int MOUSE_INDEX = 28423250;
+
+    private List<Vector2> _vertices;
 	private List<float> _animationDiff;
 	private bool _dirty;
 
@@ -12,12 +15,11 @@ public class RopeController : MonoBehaviour {
     public List<Vector2> initialBumps = new List<Vector2>();
 
     Camera _gameCamera;
-	bool _lastMouseState = false;
-	Vector2 _grabStart;
-    bool _grabbing;
+    bool _lastMouseState = false;
 
+    Dictionary<int, Vector2> _grabPoints = new Dictionary<int, Vector2>();
 
-	public List<Vector2> GetVertices() { return _vertices; }
+    public List<Vector2> GetVertices() { return _vertices; }
 	public bool GetDirty() { return _dirty; }
 
 	// Use this for initialization
@@ -26,7 +28,8 @@ public class RopeController : MonoBehaviour {
 		_gameCamera = Camera.main;
         _vertices = new List<Vector2>();
         _ResetBumps();
-		GameplayManager.instance.AddRopeController(this);
+        GameplayManager.instance.AddRopeController(this);
+        Input.simulateMouseWithTouches = false;
     }
 
     void _ResetBumps()
@@ -62,15 +65,15 @@ public class RopeController : MonoBehaviour {
         }
     }
 
-    void _OnMouseDown()
+    void _OnTouchDown(int index, Vector2 touch)
     {
         if (!userInputEnabled) { return; }
-        _grabStart = transform.InverseTransformPoint(_gameCamera.ScreenToWorldPoint(Input.mousePosition));
-		_grabbing = false;
+        Vector2 grabStart = transform.InverseTransformPoint(_gameCamera.ScreenToWorldPoint(touch));
+		bool grabbing = false;
 
 		int count = _vertices.Count;
-		float grabX = _grabStart.x;
-		float grabY = _grabStart.y;
+		float grabX = grabStart.x;
+		float grabY = grabStart.y;
 
 		float cameraLeft = -_gameCamera.orthographicSize * Screen.width / Screen.height;
 		float cameraRight = -cameraLeft;
@@ -78,13 +81,39 @@ public class RopeController : MonoBehaviour {
 		for (int i = 1; i < count; i++) {
 			if (_vertices[i - 1].x <= grabX && grabX < _vertices[i].x) {
 				float alpha = (grabX - _vertices[i - 1].x) / (_vertices[i].x - _vertices[i - 1].x);
-				float y = _vertices[i - 1].y + alpha * (_vertices[i].y - _vertices[i - 1].y);
-				if (isTop ^ (grabY <= y)) {
-					_grabbing = true;
+                float y = _vertices[i - 1].y + alpha * (_vertices[i].y - _vertices[i - 1].y);
+                y += (isTop ? -1.0f : 1.0f) * gameSettings.ropeGrabTreshold;
+                if (isTop ^ (grabY <= y)) {
+					grabbing = true;
 				}
 				break;
 			}
-		}
+        }
+
+        if (grabbing) {
+            _grabPoints[index] = grabStart;
+        }
+    }
+
+    void _OnTouchMove(int index, Vector2 touch)
+    {
+        if (!_grabPoints.ContainsKey(index)) { return; }
+
+
+        Vector2 grabPoint = _grabPoints[index];
+        Vector2 dragPoint = transform.InverseTransformPoint(_gameCamera.ScreenToWorldPoint(touch));
+		float deltaY = dragPoint.y - grabPoint.y;
+        grabPoint.y = dragPoint.y;
+        _grabPoints[index] = grabPoint;
+
+        BendRope(grabPoint.x, deltaY);
+
+		_dirty = true;
+	}
+
+    void _OnTouchUp(int index)
+    {
+        _grabPoints.Remove(index);
     }
 
     public void BendRope(float positionX, float deltaY)
@@ -101,21 +130,6 @@ public class RopeController : MonoBehaviour {
 		}
     }
 
-	void _OnMouseMove () {
-		if (!_grabbing) { return; }
-
-		Vector2 dragPoint = transform.InverseTransformPoint(_gameCamera.ScreenToWorldPoint(Input.mousePosition));
-		float deltaY = dragPoint.y - _grabStart.y;
-		_grabStart.y = dragPoint.y;
-
-		BendRope(_grabStart.x, deltaY);
-
-		_dirty = true;
-	}
-
-	void _OnMouseUp () {
-		_grabbing = false;
-	}
 
 	void FixedUpdate () {
 		_dirty = false;
@@ -124,12 +138,28 @@ public class RopeController : MonoBehaviour {
 		if (_lastMouseState != mouseState) {
 			_lastMouseState = mouseState;
 			if (mouseState) {
-				_OnMouseDown();
-			} else {
-				_OnMouseUp();
+                _OnTouchDown(MOUSE_INDEX, Input.mousePosition);
+            } else {
+				_OnTouchUp(MOUSE_INDEX);
 			}
 		} else if (mouseState) {
-			_OnMouseMove();
+            _OnTouchMove(MOUSE_INDEX, Input.mousePosition);
+        }
+
+        for (int i = 0; i < Input.touchCount; i++) {
+            Touch touch = Input.GetTouch(i);
+            switch (touch.phase) {
+                case TouchPhase.Began:
+                    _OnTouchDown(touch.fingerId, touch.position);
+                    break;
+                case TouchPhase.Moved:
+                    _OnTouchMove(touch.fingerId, touch.position);
+                    break;
+                case TouchPhase.Canceled:
+                case TouchPhase.Ended:
+                    _OnTouchUp(touch.fingerId);
+                    break;
+            }
         }
 
         if (_animationDiff == null) { return; }
